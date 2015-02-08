ffe354529bc6e60076aa7cb0ce59a134d85571cf
tuahk <kristian.jarvenpaa@gmail.com>
2015-02-08 21:55:16 +0200

    Adding files that I forgot from last commit

---
+++ recipes/docker.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: prometheus
+# Recipe:: docker
+#
+
+include_recipe 'docker'
+
+docker_image 'prom/prometheus' 
+
+docker_container 'prom/prometheus' do
+  detach true
+  port '9090:9090'
+end
