50cf4e54cddc16be12482f99b5048b7c7e8fdc3b
tuahk <kristian.jarvenpaa@gmail.com>
2015-02-09 10:48:02 +0200

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'prometheus-chef'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Kristian Järvenpää'
+maintainer_email 'kristian.jarvenpaa@gmail.com'
+license          'Apache2'
 description      'Installs/Configures prometheus-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
