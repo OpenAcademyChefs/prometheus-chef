2d532a7cad96266e1e654575b193b4d0f20a7166
tuahk <kristian.jarvenpaa@gmail.com>
2015-02-11 01:10:59 +0200

    Added attributes and configuration template

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,18 @@
 default['docker']['image_cmd_timeout'] = 600
+
+
+default['prometheus']['install_type'] = 'docker'
+default['prometheus']['port'] = '9090'
+
+default['prometheus']['scrape_interval'] = "10s"
+default['prometheus']['evaluation_interval'] = "10s"
+
+default['prometheus']['labels'] = [['service','api']]
+default['prometheus']['rulefiles'] = []
+
+default['prometheus']['jobs'] = [
+  {'name' => 'prometheus', 'scrape_interval' => '5s', 'scrape_timeout' => false,
+   'sd_name' => false, 'sd_refresh_interval' => false, 'metrics_path' => '/metrics',
+   'target_group' => ["http://localhost:9090/metrics"]} 
+
+]
--- recipes/docker.rb
+++ recipes/docker.rb
@@ -7,8 +7,14 @@ include_recipe 'docker'
 
 docker_image 'prom/prometheus' 
 
+template "/prometheus.conf" do
+  source "prometheus.conf.erb"
+  notifies :redeploy, 'docker_container[prom/prometheus]', :immediately
+end
+
 docker_container 'prom/prometheus' do
   detach true
   port '9090:9090'
-  env '-p'
+  env '-p -v'
+  volume '/prometheus.conf:/prometheus.conf'
 end
