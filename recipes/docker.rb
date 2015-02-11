8f7016795ced71f6c438e7048cf7b637f3964421
tuahk <kristian.jarvenpaa@gmail.com>
2015-02-11 23:42:14 +0200

    Just to make it converge

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -9,12 +9,11 @@ docker_image 'prom/prometheus'
 
 template "/prometheus.conf" do
   source "prometheus.conf.erb"
-  notifies :redeploy, 'docker_container[prom/prometheus]', :immediately
 end
 
 docker_container 'prom/prometheus' do
   detach true
-  port node['prometheus']['port'] 
+  port '9090:9090' 
   env '-p -v'
   volume '/prometheus.conf:/prometheus.conf'
 end
