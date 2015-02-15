e82cf44dc88add4a25a7dd924121f64aefa7e402
tuahk <kristian.jarvenpaa@gmail.com>
2015-02-15 09:53:45 +0200

    Install_path attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,14 @@
+# 
+# Cookbook Name:: prometheus-chef
+# Attribute:: default
+
+
 default['docker']['image_cmd_timeout'] = 600
 
 
 default['prometheus']['install_type'] = 'source'
 default['prometheus']['port'] = '9090:9090'
+default['prometheus']['install_path'] = "#{Chef::Config[:file_cache_path]}/prometheus"
 
 default['prometheus']['scrape_interval'] = "10s"
 default['prometheus']['evaluation_interval'] = "10s"
--- recipes/source.rb
+++ recipes/source.rb
@@ -1,34 +1,34 @@
-#
+# 
 # Cookbook Name:: prometheus
 # Recipe:: source 
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, Kristian Järvenpää
 #
 # All rights reserved - Do Not Redistribute
 
-execute "apt-get update" do
-  action :nothing
-end.run_action(:run)
+#execute "apt-get update" do
+#  action :nothing
+#end.run_action(:run)
 
 %w[build-essential curl git gzip mercurial sed].each do |pkg|
   package pkg
 end
 
-directory "#{Chef::Config[:file_cache_path]}/prometheus"
+directory node['prometheus']['install_path']
 
-git "#{Chef::Config[:file_cache_path]}/prometheus" do
+git node['prometheus']['install_path'] do
   repository 'https://github.com/prometheus/prometheus.git'
   action :sync
 end
 
 bash 'install_prometheus' do
-  cwd "#{Chef::Config[:file_cache_path]}/prometheus"
+  cwd node['prometheus']['install_path']
   code <<-EOF
   make build
   EOF
 end
 
-template "#{Chef::Config[:file_cache_path]}/prometheus/prometheus.conf" do
+template "#{node['prometheus']['install_path']}/prometheus.conf" do
   source "prometheus.conf.erb"
   notifies :restart, "service[prometheus]", :delayed
 end
@@ -36,13 +36,14 @@ end
 template "/etc/init.d/prometheus" do
   source "init.erb"
   variables ({
-    :path => "#{Chef::Config[:file_cache_path]}/prometheus/prometheus",
-    :flag => "-config.file=#{Chef::Config[:file_cache_path]}/prometheus/prometheus.conf" 
+    :path => "#{node['prometheus']['install_path']}/prometheus",
+    :flag => "-config.file=#{node['prometheus']['install_path']}/prometheus.conf" 
   })
   mode '0755'
 end
 
 service "prometheus" do
+  supports :restart => true
   action :start
 end
 
